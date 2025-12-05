2e27a2df0762cb350fa5336643dddc8b271db696
Aron Loreskär <arolor07301@edu.halmstad.se>
2025-12-05 10:31:18 +0100

    feat: insertion sort

--- algorithms.py
+++ algorithms.py
@@ -21,9 +21,17 @@ class Algorithms:
                     sorted_data[j] = temp
         return sorted_data    
     def insertion_sort(unsorted_data):
-        pass
-
-unsorted_data = [4,7,9,3,2,8,1,5,6,0]
-
-print(Algorithms.bubble_sort(unsorted_data))
-print(Algorithms.selection_sort(unsorted_data))
+        sorted_data = unsorted_data
+        
+        for i in range(1, len(sorted_data)):
+            element = sorted_data[i]
+            key = i
+          
+            for j in range(i-1, -1, -1):
+                if(sorted_data[j] > element):
+                    sorted_data[j+1] = sorted_data[j] 
+                    key-=1
+                else:
+                    break
+            sorted_data[key] = element
+        return sorted_data
