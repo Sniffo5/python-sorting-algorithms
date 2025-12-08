8951c37779bca3ced4653531cdc17417977f740b
Aron Loreskär <arolor07301@edu.halmstad.se>
2025-12-08 12:01:18 +0100

    feat: tests + printing

--- tests.py
+++ tests.py
@@ -1,6 +1,46 @@
 from algorithms import Algorithms
 from list_generation import Gen
+from time import time
+import math
+length = 1000
+unique_elements = 100
+random_elements = 100
 
-unsorted_data = Gen.random(100,1,500)
+generation_methods = [Gen.random(length),Gen.random_narrow(length,unique_elements),Gen.random_semi(length, random_elements ),Gen.sorted_reverse(length)]
+sorting_methods = [Algorithms.bubble_sort,Algorithms.selection_sort,Algorithms.insertion_sort, sorted]
 
-print(Algorithms.insertion_sort(unsorted_data[:]))
+sorting_overview = []
+
+for i in range(len(generation_methods)):
+    sorting_times = [[],[],[],[]]
+    unsorted_data = generation_methods[i]
+    for j in range(len(sorting_methods)):
+        for k in range(3):
+            start_time = time()
+            sorting_methods[j](unsorted_data[:])
+            end_time = time()
+            sorting_times[j].append(end_time-start_time)
+        sorting_times[j] = sum(sorting_times[j])/len(sorting_times[j])
+    sorting_overview.append(sorting_times)
+    
+sorting_titles = ["Bubble Sort", "Selection Sort", "Insertion Sort", "Python Sort"]
+generation_titles = ["Random", "Few Unique", "Semi Sorted", "Sorted Reverse"]
+
+table = "\t\t\t"
+
+for i in range(len(generation_titles)):
+    table += generation_titles[i]
+    if (i < 1):
+        table += "\t\t\t\t"
+    else:
+        table += "\t\t\t"
+
+for i in range(len(sorting_titles)):
+    table += "\n"
+    table += sorting_titles[i]
+    for j in range(len(sorting_overview[i])):
+        table += "\t\t"
+        table += str(sorting_overview[j][i])
+        
+
+print(table)
