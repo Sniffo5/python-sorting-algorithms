7318928022d80f3356e9644f5bd9fcef044eb757
Aron Loreskär <arolor07301@edu.halmstad.se>
2025-12-08 12:00:57 +0100

    feat: generation methods

--- list_generation.py
+++ list_generation.py
@@ -4,9 +4,14 @@ class Gen:
         return(random.sample(range(min,max),len))
         
     def random_semi(len, randomness):
-        pass
-    def random_narrow(len):
-        pass
-    def sorted_reverse(len):
+        lista = list(range(1,len))
+        for i in range(randomness+1):
+            lista[random.randrange(len-1)] = lista[random.randrange(len-1)]
+        return lista
+    
+    def random_narrow(len,unique):
+        unique_list = list(range(0,unique))
+        return(random.choices(unique_list,k=len))
         
-        pass
+    def sorted_reverse(len):
+        return(list(range(len, 0, -1)))
